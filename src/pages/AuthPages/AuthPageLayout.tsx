2ed30564af93bd6ce85575b3024f0e3efda79d82
@chisanupong <chisanupong.limsakul@gmail.com>
2025-03-23 11:39:26 +0700

    public/images/logo/company.png

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import GridShape from "../../components/common/GridShape";
-import { Link } from "react-router";
+// import GridShape from "../../components/common/GridShape";
+// import { Link } from "react-router";
 import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
 
 export default function AuthLayout({
@@ -9,28 +9,15 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
-      <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
-        {children}
-        <div className="items-center hidden w-full h-full lg:w-1/2 bg-brand-950 dark:bg-white/5 lg:grid">
-          <div className="relative flex items-center justify-center z-1">
+    <div className=" p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
+      <div className=" block flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
+        {/* <div className="items-center hidden w-full h-full lg:w-1/2  dark:bg-white/5 lg:grid"> */}
+          {/* <div className=" flex items-center justify-center z-1"> */}
             {/* <!-- ===== Common Grid Shape Start ===== --> */}
-            <GridShape />
-            <div className="flex flex-col items-center max-w-xs">
-              <Link to="/" className="block mb-4">
-                <img
-                  width={231}
-                  height={48}
-                  src="/images/logo/auth-logo.svg"
-                  alt="Logo"
-                />
-              </Link>
-              <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
-              </p>
-            </div>
-          </div>
-        </div>
+            {/* <GridShape /> */}
+          {/* </div> */}
+        {/* </div> */}
+        {children}
         <div className="fixed z-50 hidden bottom-6 right-6 sm:block">
           <ThemeTogglerTwo />
         </div>
