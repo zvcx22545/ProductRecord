52928d80c7a719d83ee99571d8cfa370d5a652ec
@chisanupong <chisanupong.limsakul@gmail.com>
2025-04-16 00:27:31 +0700

    55

--- src/pages/UserProfiles.tsx
+++ src/pages/UserProfiles.tsx
@@ -27,7 +27,7 @@ export default function UserProfiles() {
     (async () => {
       try{
         console.log('user_id is', user_id)
-        const {data} = await axios.get(`http://localhost:8000/api/authen/getUserByid/${user_id}`)
+        const {data} = await axios.get(`https://product-record-backend.vercel.app/api/authen/getUserByid/${user_id}`)
         if (data.status === true) {
           console.log("set data -------*", data.row)
         setUserProfile(data.row)
